065767ba9a904826b1d0ceb600c898e14d01c8bd
BaC0To <vasil.v.kirov@gmail.com>
2023-09-14 10:39:37 +0300

    add logger

--- factorial.py
+++ factorial.py
@@ -1,15 +1,39 @@
 import math
+import time
+import logging
+import os
+
+logfile_dir = "C:\\Appl\\BG0VVK_Work\\factorial-calc\\log\\"
+logfile_name = "logfile.log"
+logfile_path = logfile_dir+logfile_name
+if not os.path.exists(logfile_path):
+    open(logfile_path, 'w').close()
+
+# create a logger
+logging.basicConfig(filename=(logfile_path),filemode="at+",level=logging.DEBUG)
+logger = logging.getLogger()
+
 
 def factorial_calc(n):
     """Function that calculates the factorial value
     param: n: int
     return: result: int"""
-    if type(n) != int:
-        raise TypeError("The input value for factorial calculation must an integer")
     
-    elif n < 0 :
-        raise ValueError("The input value for factorial calculation can't be negative")
-    
-    result = math.factorial(n)
-    print(f"The factorial of {n} is {result}")
-    return result
+    try:
+        start_time = time.time()
+        result = math.factorial(n)
+    except (TypeError, ValueError) as exc:
+        logger.error(exc)
+        raise
+    else:
+        message = f"The factorial of {n} is {result}"
+        print(message)
+        logger.info(message)
+        return result
+    finally:
+        end_time = time.time()
+        delta_time = abs(end_time - start_time)
+        logger.info(f"The computing time is: {delta_time}")
+
+
+#data = factorial_calc(5)
--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -31,13 +31,18 @@ class TestFactorial(unittest.TestCase):
         def test_factorial_of_wrong_type(self):
                 #Test factorial function with numbers that are not of type int
                 test_number1 = (3+5j)
-                self.assertRaises(TypeError, factorial_calc, test_number1)
-                test_number2 = True
+                self.assertRaises(TypeError, factorial_calc, test_number1)                
+                test_number2 = "five"
                 self.assertRaises(TypeError, factorial_calc, test_number2)
-                test_number3 = "five"
-                self.assertRaises(TypeError, factorial_calc, test_number3)
-        
 
+        def test_factorial_of_boolean_values(self):
+                #Test factorial function with bool True/False
+                test_value1 = True
+                expected_value = 1
+                self.assertEqual(factorial_calc(test_value1),expected_value)
+                test_value2 = False
+                self.assertEqual(factorial_calc(test_value2),expected_value)
+
+                
 if __name__ == '__main__':
     unittest.main()
-    
